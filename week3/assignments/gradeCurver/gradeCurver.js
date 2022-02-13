b2fcc93a08afc423d147ff1c1faf9aa9e1c3379f
Stephanie Freeman <stephanie.freeman@bryanuniversity.edu>
2022-02-13 13:08:59 -0800

    added assignments

--- week3/assignments/gradeCurver/gradeCurver.js
+++ week3/assignments/gradeCurver/gradeCurver.js
@@ -1,21 +1,27 @@
-// declare each of the variables marked with "**" in the appropriate scope and using the appropriate type
-
 // create an event listener that calls the curveGrade() function when the Curve It!! button is clicked
+let curveItBtn = document.querySelector("#submit");
+curveItBtn.addEventListener("click", curveGrades);
 
 // create an event listener that resets the scores and grades to their defaults when the Reset button is clicked
+let scores = document.querySelector("#scores");
+let grades = document.querySelector("#grades");
+document.querySelector("#reset").addEventListener("click", () => {
+    scores.value = "";
+    grades.innerHTML = "Curved Grades Show Here";
+});
 
 function applyBell(grade, index, ary) {
     switch (true) {
-        case grade >= (mean + (gradeSlice * 2)): 
+        case grade >= (mean + (gradeSlice * 2)):
             ary[index] = 'A'
             break
-        case grade >= (mean + gradeSlice): 
+        case grade >= (mean + gradeSlice):
             ary[index] = 'B'
             break
         case grade >= (mean):
             ary[index] = 'C'
             break
-        case grade >= (mean - gradeSlice): 
+        case grade >= (mean - gradeSlice):
             ary[index] = 'D'
             break
         default:
@@ -26,43 +32,32 @@ function applyBell(grade, index, ary) {
 
 function convertArray(obj) {
     ary = obj.value.split(',')
-    ary = ary.map(function (x) {
+    ary = ary.map(function(x) {
         return parseInt(x)
     })
     return ary
 }
-
+// declare each of the variables marked with "**" in the appropriate scope and using the appropriate type
 // Condense the number of lines within the curveGrade() function as much as possible by converting 
 // the functions to arrow functions. You can also condense the number of lines by combining some 
 // separate lines of code into single lines. It currently has 18 lines of code. Without counting  
 // empty lines, can you get the number of lines down to 8?
 
 function curveGrades() {
-    **sum = function (accumulator, currentValue) {
-        return accumulator + currentValue
-    }
+    let sumGrades = array => (array.reduce((accumulator, currentValue) => (accumulator + currentValue)));
 
-    **sumGrades = function(array) {
-        return array.reduce(sum)
-    }
+    let aryGrades = convertArray(document.querySelector("#scores"));
 
-    **aryGrades = convertArray(document.querySelector('#scores'))
+    let minGrade = aryGrades.reduce((a, b) => (Math.min(a, b)));
 
-    **minGrade = aryGrades.reduce(function(a, b) {
-        return Math.min(a, b)
-    })
-    
-    **maxGrade = aryGrades.reduce(function(a, b) {
-        return Math.max(a, b)
-    })
-    
-    **mean = sumGrades(aryGrades) / aryGrades.length
+    let maxGrade = aryGrades.reduce((a, b) => (Math.max(a, b)));
 
-    **range = maxGrade - minGrade
+    mean = sumGrades(aryGrades) / aryGrades.length;
 
-    gradeSlice = range / 5
+    gradeSlice = (maxGrade - minGrade) / 5;
 
-    aryGrades.forEach(applyBell)
+    aryGrades.forEach(applyBell);
 
     // write the value of aryGrades to the grades div in the HTML document
-}
+    grades.innerText = aryGrades.join();
+};
--- week3/assignments/starwars/starwars.js
+++ week3/assignments/starwars/starwars.js
@@ -1,21 +1,24 @@
 // declare any necessary variables
 
+let queryType;
+let itemID;
+
 // define a function called 'fetchData()' that passes the values from 
 // the 'queryType' and 'itemID' elements in starwars.html to the function 
 // called 'getFromSWAPI()'
 
+function fetchData() {
+    queryType = document.querySelector("#queryType").value;
+    itemID = document.querySelector("#itemID").value;
+    getFromSWAPI(queryType, itemID);
+}
+
 function getFromSWAPI() {
     // assign values to any necessary variables
     fetch(`https://swapi.dev/api/${queryType}/${itemID}`)
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function(data){
-        updateInfo(data)
-    })
-    .catch(function(err) {
-        console.warn(err)
-    })
+    .then(response => response.json())
+    .then(data => updateInfo(data))
+    .catch(err => console.warn(err))
 }
 
 // create a new function called 'updateInfo()' that receives the data from 
@@ -23,3 +26,11 @@ function getFromSWAPI() {
 //labels to 'dataLabel1' and 'dataLabel2' elements in starwars.html, as well
 // as the appropriate values from the data object to the 'dataValue1' and 
 // 'dataValue2' elements in starwars.html.
+
+function updateInfo(data) {
+    const keys = Object.keys(data);
+    document.querySelector("#dataLabel1").innerText = keys[0];
+    document.querySelector("#dataValue1").innerText = data[keys[0]];
+    document.querySelector("#dataLabel2").innerText = keys[5];
+    document.querySelector("#dataValue2").innerText = data[keys[5]];
+}
