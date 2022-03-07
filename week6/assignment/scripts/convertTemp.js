cabc95c97c98908e84674f398ab220a20f1df8b2
Stephanie Freeman <stephanie.freeman@bryanuniversity.edu>
2022-03-06 22:26:06 -0800

    add assignments

--- week6/assignment/scripts/convertTemp.js
+++ week6/assignment/scripts/convertTemp.js
@@ -1,2 +1,9 @@
 // This script should return the results of a function that converts the temperature provided by the api (Kelvin).
 // Depending on the location of the city, the function should return degrees in either fahrenheit or centigrade.
+export default function localTemp(kelvin, data) {
+    if (data.sys.country == 'US') {
+        return `${(9/5 * (kelvin - 273.15) + 32).toFixed(0)}F`;
+    } else {
+        return `${(kelvin - 273.15).toFixed(0)}C`;
+    }
+}
--- week6/assignment/scripts/weather.js
+++ week6/assignment/scripts/weather.js
@@ -1,15 +1,47 @@
 // import the convertTemp.js and getDaylight.js scripts with their default export
+import localTemp from "./convertTemp.js ";
+import blueBlack from "./getDayight.js"
 
 // declare any variables needed
+let data, cityName;
+const temp = document.querySelector('#tempData');
+const humidity = document.querySelector('#humidData');
+const conditions = document.querySelector('#conditionsData');
+
 
 // create an event listener for the click of the goBttn that calls a function to fetch the weather data
+document.querySelector('#goBttn').addEventListener('click', () => {
+    cityName = document.getElementById('city').value;
+    queryAPI(cityName);
+});
+
 
 // create a function that calls the function that queries the api 
 // and then creates promises that will call a function to write the weather data to the HTML page        
+function queryAPI(city) {
+    fetchData(city)
+        .then(function(data) {
+            toLocal(data);
+        })
+        .catch(function(err) {
+            console.warn(err);
+        })
+}
 
 // use an asynchronous call to fetches the current weather for the specified city, awaits it, 
 // and returns the resulting data
+const fetchData = async(city) => {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fd039ae60a44c9f409cbfdb6298e8609`);
+    data = await response.json();
+    return data;
+}
 
 // create a function that writes the temperature (using local units), humidity, and conditions
 // this function should also change the background color of the weather app to blue during the daylight
 // hours at the specified city
+function toLocal(data) {
+    temp.innerText = localTemp(data.main.temp, data);
+    humidity.innerText = `${data.main.humidity}%`;
+    conditions.innerText = data.weather[0].main;
+    document.querySelector('.weatherWrapper').style.backgroundColor = blueBlack(data);
+}
